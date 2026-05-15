f3b20ddfa40c9d967ad1114f87c6502f9f9ba9af
Lauti <lautarosaintmartin2@gmail.com>
2026-05-15 20:24:26 -0300

    Actualizacion: migracion de prisma a la base de datos y arreglos a la logica de users

---
+++ src/users/users.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private prismaService: PrismaService) { }
+
+  async create(createUserDto: CreateUserDto) {
+
+    try {
+      return await this.prismaService.user.create({
+        data: createUserDto
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async findAll() {
+    try {
+      return await this.prismaService.user.findMany();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async findOne(id: number) {
+    try {
+      return await this.prismaService.user.findUnique(
+        {
+          where: {
+            id
+              : id
+          }
+        }
+      );
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id)
+
+    try {
+
+
+      if (!user) {
+        throw new NotFoundException('User not found')
+      }
+      return await this.prismaService.user.update({
+        where: {
+          id,
+        },
+        data: updateUserDto
+
+      })
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+
+  async remove(id: number) {
+    return await this.prismaService.user.delete({
+      where: {
+        id,
+      }
+    })
+  } catch(error) {
+    console.log(error)
+  }
+}
