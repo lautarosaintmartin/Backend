c13d10bf219db563fe89207a12ecae8fc8c07008
Lauti <lautarosaintmartin2@gmail.com>
2026-05-23 11:48:06 -0300

    Actualización: agregar modelo de productos con controladores, servicios y DTOs en el módulo de productos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, CategoriesModule],
+  imports: [UsersModule, CategoriesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
